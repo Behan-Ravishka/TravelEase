187d269e9ff20d7ab03bb9876108d99da86760e3
M P Behan Ravishka Perera <behanravishka03@gmail.com>
2024-10-11 23:20:37 +0530

    update

--- index.php
+++ index.php
@@ -67,22 +67,8 @@ $isLoggedIn = isset($_SESSION['user_id']); // assuming 'user_id' is set when log
                 echo '<a href="signup.php" class="hover-effect px-4 py-2 rounded-full text-white bg-gradient-to-r from-pink-500 to-purple-500">Sign Up</a>';
             }
             ?>
-            <!-- Notification Center Button -->
-        <button id="notification-center-btn" class="relative p-2 bg-transparent hover:bg-blue-600 hover:bg-opacity-50 transition-all duration-300 rounded-full text-white focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6 6 0 10-12 0v3.158c0 .538-.214 1.055-.595 1.437L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-            </svg>
-            <!-- Notification Count Badge -->
-            <span id="notification-count" class="absolute top-0 right-0 text-xs bg-red-600 rounded-full w-5 h-5 flex items-center justify-center">0</span>
-        </button>
-        <!-- Notification Dropdown (Initially Hidden) -->
-<div id="notification-dropdown" class="absolute right-0 mt-2 w-64 bg-white shadow-lg rounded-lg hidden">
-    <div id="notification-list" class="p-4">
-        <!-- Notifications will be dynamically added here -->
-    </div>
-</div>
-
             
+            </div>  
         </div>
     </div>
 </header>
